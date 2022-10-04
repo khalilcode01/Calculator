bee4ea87989dbdaf8dba255eecc6708d0eddaac4
khalil sadi <khalilcode01@gmail.com>
2022-10-04 05:43:45 +0300

    modified:   myScript.js Add operate function

---
+++ myScript.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(op, a, b) {
+    if(op === '+') 
+    return add(a, b);
+    else if(op === '-')
+    return subtract(a, b);
+    else if(op === '*')
+    return multiply(a, b);
+    else if(op === '/');
+    return divide(a, b);  
+}
