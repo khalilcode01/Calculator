a194f99d85a9a5fc8512ccebf51950124061e502
khalil sadi <khalilcode01@gmail.com>
2022-10-05 23:26:25 +0300

    Add eventListener to the buttons

--- myScript.js
+++ myScript.js
@@ -20,3 +20,32 @@ function operate(op, a, b) {
     else if(op === '/');
     return divide(a, b);  
 }
+let numbers = document.querySelectorAll('.num');
+let screen = document.querySelector('.screen');
+let display = document.createElement('div');
+display.classList.add('content');
+let operators = document.querySelectorAll('.op');
+let proNum = document.querySelector('.content');
+let clear = document.querySelector('.clear');
+let pool = 0;
+let value = 0;
+numbers.forEach(buttons => {
+    buttons.addEventListener('click', () => {
+        display.textContent += buttons.textContent;
+        value = buttons.textContent;
+        screen.appendChild(display);
+       
+    });
+});
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        display.textContent += op.textContent;
+        let value = buttons.textContent;
+        screen.appendChild(display);
+        
+    });
+
+});
+clear.addEventListener('click', () =>{
+    display.textContent = '';
+});
